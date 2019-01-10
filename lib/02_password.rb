7e2caa00f12a091ef6618b7bba80d246a6db78b5
Arbousied <46084242+Arbousied@users.noreply.github.com>
2019-01-10 13:14:49 +0100

    upload password

---
+++ lib/02_password.rb
@@ -0,0 +1,28 @@
+def sign_up
+  puts"Définis ton mot de passe poto :"
+  print">"
+  mdp = gets.chomp
+  return mdp
+end
+
+def welcome_screen
+  puts"Welcome home, baby. Donald Trump est une femme, c'est la NSA qui l'a dit."
+end
+
+def login(mdp)
+  puts"Confirme ton mot de passe"
+  print">"
+
+  puts "C'est pas le bon mot de passe poto."
+  while gets.chomp != mdp
+  welcome_screen
+
+
+end
+
+def perform
+  mdp = sign_up
+  login(mdp)
+end
+
+perform
